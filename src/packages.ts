66f2ecad0a7b59397ad9bab74a51c6001a15c172
Russell Jones <jonesrussell42@gmail.com>
2020-10-29 14:12:09 -0400

    WIP

--- src/packages.ts
+++ src/packages.ts
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 
 import { readFileSync } from "fs";
-import program from "commander";
+import program, { on } from "commander";
 import { Validator } from "jsonschema";
 import chalk from "chalk";
+import globalModules from "global-modules";
+import { Table } from "console-table-printer";
+
+/**
+ * Convert array into associative array for display in table
+ * @param deps array of dependencies from package.json
+ */
+function forTable(deps: [string, string][], ...labels: string[]) {
+  return Object.assign(
+    [],
+    deps.map((item) => {
+      labels[0]: item[0],
+      labels[1]: item[1],
+    })
+  );
+}
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", `${err.message}`);
+  process.exit(1);
+});
 
 const cwd: string = process.cwd();
-// const scriptwd: string = process;
-const execPath = process.execPath;
-console.log(``);
-const schemaFilename = `./package.schema.json`;
+
+const schemaFilename = `${globalModules}/@jonesrussell/pkg-info/package.schema.json`;
 const filename = `${cwd}/package.json`;
 
 let fileSchema: string;
@@ -38,6 +57,7 @@ const schema = JSON.parse(fileSchema);
 // Set the program version
 program.version(data.version);
 
+// Validate package.json against schema
 const v = new Validator();
 const validated = v.validate(data, schema);
 
@@ -49,12 +69,28 @@ if (!validated.valid) {
 /**
  * Show deps
  */
-const table = console.table;
+const deps: [string, string][] = Object.entries(data.dependencies);
+const devDeps: [string, string][] = Object.entries(data.devDependencies);
 
-console.group(chalk.blue("Dependencies"));
-table(data.dependencies);
-console.groupEnd();
+const p = new Table({
+  title: "dependencies",
+  columns: [{ name: "Name" }, { name: "Version" }],
+});
 
-console.group(chalk.blue("Dev Dependencies"));
-table(data.devDependencies);
-console.groupEnd();
+const rows = forTable(deps, "Name", "Version");
+p.addRows(rows);
+
+p.printTable();
+
+/**
+ * Show devDependencies
+ */
+const p2 = new Table({
+  title: "devDependencies",
+  columns: [{ name: "Name" }, { name: "Version" }],
+});
+
+const rows2 = forTable(devDeps, "Name", "Version");
+p2.addRows(rows2);
+
+p2.printTable();
