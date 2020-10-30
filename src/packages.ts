5a2152af4f0169489b0457ea4894bf8dcb7df55a
Russell Jones <jonesrussell42@gmail.com>
2020-10-30 18:47:29 -0400

    Fix output

--- src/packages.ts
+++ src/packages.ts
@@ -11,20 +11,21 @@ import { Table } from "console-table-printer";
  * Convert array into associative array for display in table
  * @param deps array of dependencies from package.json
  */
-function forTable(deps: [string, string][], ...labels: string[]) {
-  return Object.assign(
-    [],
-    deps.map((item) => {
-      labels[0]: item[0],
-      labels[1]: item[1],
-    })
-  );
+function forTable(
+  deps: [string, string][],
+  name = "Name",
+  version = "Version"
+) {
+  return Object.entries(deps).reduce((result, [n, v]) => {
+    return Object.assign(result, { name: n, version: v });
+  }, {});
+  // return new Map(deps.map((x) => [{ name: x[0], version: x[1] }]));
 }
 
-process.on("uncaughtException", (err) => {
+/*process.on("uncaughtException", (err: any) => {
   console.error("Uncaught Exception:", `${err.message}`);
   process.exit(1);
-});
+});*/
 
 const cwd: string = process.cwd();
 
@@ -38,15 +39,16 @@ let file: string;
  * Try opening package.json and schema
  */
 try {
-  file = readFileSync(filename, "utf-8");
+  fileSchema = readFileSync(schemaFilename, "utf-8");
 } catch (err) {
-  throw new Error(err);
+  // throw console.error(err.name, "Cannot open package.schema.json");
+  throw new Error("Cannot open package.schema.json");
 }
 
 try {
-  fileSchema = readFileSync(schemaFilename, "utf-8");
+  file = readFileSync(filename, "utf-8");
 } catch (err) {
-  throw new Error(err);
+  throw console.error(err.name, "Cannot open package.json");
 }
 
 // Convert the file contents to a JSON object
@@ -63,29 +65,42 @@ const validated = v.validate(data, schema);
 
 if (!validated.valid) {
   // console.log(chalk.blue("Invalid package.json"));
-  throw new Error("Invalid package.json");
+  throw console.error("Invalid package.json");
 }
 
 /**
  * Show deps
  */
-const deps: [string, string][] = Object.entries(data.dependencies);
-const devDeps: [string, string][] = Object.entries(data.devDependencies);
+let deps = [];
+for (let [name, version] of Object.entries(data.dependencies)) {
+  deps.push({ Name: name, Version: version });
+}
+
+let devDeps = [];
+for (let [name, version] of Object.entries(data.devDependencies)) {
+  devDeps.push({ Name: name, Version: version });
+}
 
 const p = new Table({
   title: "dependencies",
   columns: [{ name: "Name" }, { name: "Version" }],
 });
-
-const rows = forTable(deps, "Name", "Version");
-p.addRows(rows);
-
+p.addRows(deps);
 p.printTable();
+
+const p2 = new Table({
+  title: "devDependencies",
+  columns: [{ name: "Name" }, { name: "Version" }],
+});
+p2.addRows(devDeps);
+p2.printTable();
+
+process.exit(0);
 
 /**
  * Show devDependencies
  */
-const p2 = new Table({
+/*const p2 = new Table({
   title: "devDependencies",
   columns: [{ name: "Name" }, { name: "Version" }],
 });
@@ -94,3 +109,4 @@ const rows2 = forTable(devDeps, "Name", "Version");
 p2.addRows(rows2);
 
 p2.printTable();
+*/
